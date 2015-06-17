8c124e6b8f77f2652c231a7d1bc5f63b6d171198
Brandon Bloom <brandon@brandonbloom.name>
2015-06-16 23:40:36 -0700

    Proof of concept

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
 # glens
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure library designed to provide something similar to lenses (or zippers)
+for Graphs (hence the name: Graph+LENS). However, it's neither lenses or
+zippers in the formal sense.
 
 ## Usage
 
-FIXME
+Don't.
+
+But the general idea is this:
+
+```clojure
+(use 'glens.core)
+
+(-> null    ; An empty graph
+    edges   ; Accessors symbolic lens/zippers/cursors in to the graph.
+    (conj [:x :y] [:y :z])  ; These types implement standard interfaces.
+    graph)  ; And you can navigate back around with them.
+;=> #graph [#{:y :z :x} {:x #{:y}, :y #{:z :x}, :z #{:y}}]
+; Where this is ^nodes^ and ^adjacency^, but representation will change.
+```
+
+The hope is that the only extra interfaces you'll need in the general case are
+`null`, `graph`, `nodes`, and `edges`; plus the usual Clojure suspects.
+
+I have no idea if this is actually a good idea; nor how far to push it. Could
+get crazy, for example representing attributed graphs with metadata which is
+a custom map implementation that similarly tracks being part of the graph.
+
+Let me know via Twitter if this idea is at all interesting to you.
 
 ## License
 
-Copyright © 2015 FIXME
+Copyright © 2015 Brandon Bloom
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- src/glens/core.clj
+++ src/glens/core.clj
@@ -1,6 +1,221 @@
 (ns glens.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def conjs (fnil conj #{}))
+
+(defprotocol Attached
+  (-graph [this])
+  ;XXX -detatch
+  )
+
+(defprotocol HasEdges
+  (-edges [this]))
+
+(defprotocol HasNodes
+  (-nodes [this]))
+
+(defn graph [x] (-graph x))
+(defn nodes [x] (-nodes x))
+(defn edges [x] (-edges x))
+
+(defn edge-id? [x]
+  (and (vector? x) (= (count x) 2)))
+
+(deftype Graph [nodes adjacent]
+
+  Attached
+  (-graph [this]
+    this)
+
+  HasEdges
+  (-edges [this]
+    (GraphEdges. this))
+
+  HasNodes
+  (-nodes [this]
+    (GraphNodes. this))
+
+  )
+
+(deftype Node [^Graph g id]
+
+  Attached
+  (-graph [this] g)
+
+  ;XXX HasEdges
+
+  )
+
+(deftype Edge [^Graph g id]
+
+  Attached
+  (-graph [this] g)
+
+  ;XXX HasNodes
+
+  )
+
+(deftype GraphNodes [^Graph g]
+
+  Attached
+  (-graph [this] g)
+
+  clojure.lang.Counted
+
+  ;XXX clojure.lang.IEditableCollection
+  ;XXX asTransient
+
+  clojure.lang.IFn
+  (invoke [this node]
+    )
+  (invoke [this node not-found]
+    )
+
+  ;XXX clojure.lang.IHashEq
+  ;XXX (hasheq [this])
+
+  clojure.lang.IPersistentCollection
+  (cons [this obj]
+    (GraphNodes. (Graph. (conj (.nodes g) obj) (.adjacent g))))
+  (count [this]
+    (count (.nodes g)))
+  ;XXX (empty [this]
+  ;XXX   )
+  ;XXX (equiv [this x]
+  ;XXX   )
+
+  clojure.lang.IPersistentSet
+  ;XXX (contains [this x]
+  ;XXX   )
+  ;XXX (disjoin [this x]
+  ;XXX   )
+  ;XXX (get [this x]
+  ;XXX   )
+
+  ;XXX clojure.lang.Seqable
+  ;XXX (seq [this]
+  ;XXX   )
+
+  java.io.Serializable
+
+  ;XXX java.lang.Iterable
+  ;XXX (iterator [this]
+  ;XXX   )
+
+  ;XXX java.lang.Runnable
+  ;XXX (run [this]
+  ;XXX   )
+
+  ;XXX java.util.Collection
+  ;XXX (clojure.reflect/type-reflect java.util.Collection)
+
+  ;XXX java.util.Set
+  ;XXX (clojure.reflect/type-reflect java.util.Set)
+
+  ;XXX java.util.concurrent.Callable
+  ;XXX (call [this]
+  ;XXX   )
+
+  )
+
+(deftype GraphEdges [g]
+
+  Attached
+  (-graph [this] g)
+
+  clojure.lang.Counted
+
+  ;XXX clojure.lang.IEditableCollection
+  ;XXX asTransient
+
+  clojure.lang.IFn
+  (invoke [this node]
+    )
+  (invoke [this node not-found]
+    )
+
+  ;XXX clojure.lang.IHashEq
+  ;XXX (hasheq [this])
+
+  clojure.lang.IPersistentCollection
+  (cons [this obj]
+    (assert (edge-id? obj))
+    (let [[x y] obj
+          nodes (conj (.nodes g) x y)
+          adj (-> (.adjacent g)
+                  (update x conjs y)
+                  (update y conjs x))]
+      (GraphEdges. (Graph. nodes adj))))
+  ;XXX (count [this]
+  ;XXX   )
+  ;XXX (empty [this]
+  ;XXX   )
+  ;XXX (equiv [this x]
+  ;XXX   )
+
+  clojure.lang.IPersistentSet
+  ;XXX (contains [this x]
+  ;XXX   )
+  (disjoin [this obj]
+    (assert (edge-id? obj))
+    (let [[x y] obj
+          adj (-> (.adjacent g)
+                  (update x disj y)
+                  (update y disj x))]
+      (GraphEdges. (Graph. (.nodes g) adj))))
+  ;XXX (get [this x]
+  ;XXX   )
+
+  ;XXX clojure.lang.Seqable
+  ;XXX (seq [this]
+  ;XXX   )
+
+  java.io.Serializable
+
+  ;XXX java.lang.Iterable
+  ;XXX (iterator [this]
+  ;XXX   )
+
+  ;XXX java.lang.Runnable
+  ;XXX (run [this]
+  ;XXX   )
+
+  ;XXX java.util.Collection
+  ;XXX (clojure.reflect/type-reflect java.util.Collection)
+
+  ;XXX java.util.Set
+  ;XXX (clojure.reflect/type-reflect java.util.Set)
+
+  ;XXX java.util.concurrent.Callable
+  ;XXX (call [this]
+  ;XXX   )
+
+  )
+
+;XXX (deftype NodeEdges )
+;XXX (deftype EdgeNodes )
+
+(defn- graph-rep [^Graph g]
+  [(.nodes g) (.adjacent g)]) ;TODO edges instead of adjacent?
+
+(defmethod print-method Graph [^Graph x writer]
+  (print-method (tagged-literal 'graph (graph-rep x)) writer))
+
+(defmethod print-method GraphNodes [^GraphNodes x writer]
+  (print-method (tagged-literal 'nodes (graph-rep (.g x))) writer))
+
+(defmethod print-method GraphEdges [^GraphEdges x writer]
+  (print-method (tagged-literal 'edges (graph-rep (.g x))) writer))
+
+(def null (Graph. #{} {}))
+
+(comment
+
+  (-> null edges (conj [:x :y] [:y :z]) graph)
+
+  (-> null
+      nodes (conj :foo :bar)
+      graph edges (conj [:a :b] [:b :c] [:bar :baz])
+      ;(disj [:b :c])
+      )
+
+)
